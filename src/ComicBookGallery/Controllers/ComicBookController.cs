ec330e680342fa7559285c5106bbefa808594186
ddabrahim <ddabrahim@>
2018-06-05 14:55:29 +0100

    Added Detail view

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -10,17 +10,19 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail() //actionresult is the base result type used by both RedirectResult and ContentResult
         {
-            //if the day is tuesday, redirect to an other page
+            /*//if the day is tuesday, redirect to an other page
             if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
             {
                 // return new RedirectResult("/"); As our class inherit from the Controller class, we can simplyify this using the Redirect method
                 return Redirect("/");
             }
+            */
+
+            //return detail View,
+            return View();
 
 
-            //return detail page
-            return Content("Hello");
-
+            //return Content("Hello");
             /* As our class inherit from the Controller class, we can simplify this using the Content method
             return new ContentResult()
             {
