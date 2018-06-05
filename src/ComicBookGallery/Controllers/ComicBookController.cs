cdeb25a1e4962369d62a50ff119f18cb2a8a6aa9
ddabrahim <ddabrahim@>
2018-06-05 14:32:23 +0100

    Updated Detail action method to use action result types

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -8,9 +8,25 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBookController : Controller
     {
-        public string Detail()
+        public ActionResult Detail() //actionresult is the base result type used by both RedirectResult and ContentResult
         {
-            return "Hello";
+            //if the day is tuesday, redirect to an other page
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                // return new RedirectResult("/"); As our class inherit from the Controller class, we can simplyify this using the Redirect method
+                return Redirect("/");
+            }
+
+
+            //return detail page
+            return Content("Hello");
+
+            /* As our class inherit from the Controller class, we can simplify this using the Content method
+            return new ContentResult()
+            {
+                Content = "Hello"
+            };
+            */
         }
     }
 }
