13ad62f137338ab63f19e7a14344a25812ea8082
ddabrahim <ddabrahim@>
2018-06-06 10:48:47 +0100

    Updated ComicBook Model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -9,10 +9,26 @@ namespace ComicBookGallery.Models
     {
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
-        public string IssueTitle { get; set; }
+        public string IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        public string CoverImage
+        {
+           get
+           {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+           }
+        }
 
     }
 }
